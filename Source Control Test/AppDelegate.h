042918a9459d6843bc9de26300bdc9e9f9653dbf
Matthew Wong <mwong678@gmail.com>
2015-09-05 21:30:54 -0700

    Initial Commit

---
+++ Source Control Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Source Control Test
+//
+//  Created by Matthew Wong on 9/5/15.
+//  Copyright (c) 2015 Matthew Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Source Control Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Source Control Test
+//
+//  Created by Matthew Wong on 9/5/15.
+//  Copyright (c) 2015 Matthew Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
